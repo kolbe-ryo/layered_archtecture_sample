101e6f29e7c53ca257b3235f607414ff9280d898
kolbe <water.northground@gmail.com>
2023-12-15 23:15:01 +0900

    remove assert

--- lib/domain/user/entity/user.dart
+++ lib/domain/user/entity/user.dart
@@ -4,10 +4,6 @@ part 'user.freezed.dart';
 
 @freezed
 class User with _$User {
-// @AssertはAssertionErrorでcatchできる？
-  @Assert('id.isNotEmpty', 'ID cannot be empty')
-  @Assert('userName.isNotEmpty', 'UserName cannot be empty')
-  @Assert('imageUrl.isNotEmpty', 'ImageURL cannot be empty')
   const factory User({
     required String id,
     required String userName,
