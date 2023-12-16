b1f065588c314bd48f82af8c95e6fe0dfe1b755d
kolbe <water.northground@gmail.com>
2023-12-16 20:10:07 +0900

    add storage service provider

--- lib/domain/service/storage_service.dart
+++ lib/domain/service/storage_service.dart
@@ -1,5 +1,14 @@
 import 'dart:io';
 
+/// ストレージサービスプロバイダー
+/// アプリ起動時 or テスト時に本プロバイダーを override して使用してください
+final storageServiceProvider = Provider<StorageService>(
+  // 初期値を StorageServiceImpl にしてしまうと、
+  // ドメイン層がインフラ層に依存してしまうことになるので、
+  // どの層にも依存させないために未実装エラーを返却するようにしておく
+  (_) => throw UnimplementedError(),
+);
+
 abstract class StorageService {
   /// 指定した画像をストレージにアップロード
   Future<String> uploadImage({required File image});
