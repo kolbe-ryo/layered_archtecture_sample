a1425709cb54aa2d4e8806fb267d6babf54dfd82
kolbe <water.northground@gmail.com>
2023-12-17 16:14:25 +0900

    generate post repository provider

---
+++ lib/domain/post/post_repository.dart
@@ -0,0 +1,29 @@
+import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
+import 'package:riverpod/riverpod.dart';
+
+/// 投稿リポジトリプロバイダー
+/// アプリ起動時 or テスト時に本プロバイダーを override して使用してください
+final postRepositoryProvider = Provider<PostRepository>(
+  // 初期値を PostRepositoryImpl にしてしまうと、
+  // ドメイン層がインフラ層に依存してしまうことになるので、
+  // どの層にも依存させないために未実装エラーを返却するようにしておく
+  (ref) => throw UnimplementedError(),
+);
+
+/// Post テーブルに関するリポジトリ
+abstract class PostRepository {
+  /// 投稿情報追加
+  Future<void> add({required Post post});
+
+  /// 投稿情報変更
+  Future<void> update({required Post post});
+
+  /// 投稿情報削除
+  Future<void> delete({required String postId});
+
+  /// 投稿情報取得
+  Future<Post> fetch({required String postId});
+
+  /// 全投稿情報取得
+  Future<List<Post>> fetchAll();
+}
