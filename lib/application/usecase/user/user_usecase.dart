ef10bf3fb5604246133f205993308d7423824929
kolbe <water.northground@gmail.com>
2023-12-18 21:36:49 +0900

    implement signUp method

--- lib/application/usecase/user/user_usecase.dart
+++ lib/application/usecase/user/user_usecase.dart
@@ -1,3 +1,5 @@
+import 'package:layered_archtecture_sample/application/usecase/user/state/user_provider.dart';
+import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
 import 'package:riverpod/riverpod.dart';
 
 class UserUsecase {
@@ -8,5 +10,11 @@ class UserUsecase {
   Future<void> signUp({
     required String email,
     required String password,
-  }) async {}
+  }) async {
+    final userId = await _ref.read(userRepositoryProvider).signUp(
+          email: email,
+          password: password,
+        );
+    _ref.read(uidProvider.notifier).update((_) => userId);
+  }
 }
