8163ac6118c0dabaa72e7aacd3eb8a8c76c10b5f
kolbe <water.northground@gmail.com>
2023-12-18 19:56:33 +0900

    implement storage service mock

---
+++ lib/infrastructure/mocks/mock_storage_service.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
+
+class MockStorageService implements StorageService {
+  /// 指定画像を外部ストレージに保存した程にして、ダミーの保存先 URL を返却
+  @override
+  Future<String> uploadImage({required File image}) async {
+    await Future.delayed(const Duration(seconds: 2));
+    return 'https://1.bp.blogspot.com/-rb5mSYSN8pA/X6tmegQM2ZI/AAAAAAABcLw/_-n5UvfxhJItVJnKRrycKPShVDsxStrjACNcBGAsYHQ/s400/fruit_apple_yellow.png';
+  }
+}
