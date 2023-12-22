a272005a4f0d17d8fda17270f6fa2b8ce366c999
kolbe <water.northground@gmail.com>
2023-12-22 23:01:29 +0900

    implement addPost usecase

--- lib/application/usecase/post/post_usecase.dart
+++ lib/application/usecase/post/post_usecase.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import 'package:layered_archtecture_sample/domain/app_exception.dart';
 import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
+import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
+import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
 import 'package:layered_archtecture_sample/domain/user/entity/user.dart';
 import 'package:riverpod/riverpod.dart';
 
@@ -14,7 +17,25 @@ class PostUsecase {
     required File? image,
     required String comment,
     required User? user,
-  }) async {}
+  }) async {
+    if (image == null) {
+      throw const AppException('画像を選択してください');
+    }
+    if (user == null) {
+      throw const AppException();
+    }
+
+    final imageUrl = await _ref.read(storageServiceProvider).uploadImage(image: image);
+    await _ref.read(postRepositoryProvider).add(
+          post: Post(
+            id: null,
+            user: user,
+            imageUrl: imageUrl,
+            comment: comment,
+            createdAt: DateTime.now(),
+          ),
+        );
+  }
 
   /// 投稿の全件取得処理
   ///
