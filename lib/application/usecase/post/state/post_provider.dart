654cdc4a225d6a7091c55ae0ea66d7e87024def4
kolbe <water.northground@gmail.com>
2023-12-23 21:28:37 +0900

    implement post provider

--- lib/application/usecase/post/state/post_provider.dart
+++ lib/application/usecase/post/state/post_provider.dart
@@ -1,1 +1,7 @@
+import 'package:layered_archtecture_sample/application/usecase/post/post_usecase.dart';
+import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
+import 'package:riverpod/riverpod.dart';
 
+final postProvider = FutureProvider.autoDispose<List<Post>>(
+  (ref) async => ref.watch(postUsecaseProvider).fetchAll(),
+);
