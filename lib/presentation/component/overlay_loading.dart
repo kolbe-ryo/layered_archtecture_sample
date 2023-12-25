a246cc5bd82426286f82c15f66d2f5c5001366da
kolbe <water.northground@gmail.com>
2023-12-25 23:52:07 +0900

    loading widget

---
+++ lib/presentation/component/overlay_loading.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/application/state/overlay_loading_provider.dart';
+
+class OverlayLoading extends ConsumerWidget {
+  const OverlayLoading({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(overlayLoadingProvider);
+    return Visibility(
+      visible: isLoading,
+      child: Placeholder(),
+    );
+  }
+}
