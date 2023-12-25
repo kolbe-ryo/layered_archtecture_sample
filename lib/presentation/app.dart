a15ee031e4329db63bdbb4837b77a00a371a1112
kolbe <water.northground@gmail.com>
2023-12-25 23:56:47 +0900

    comment for material app builder

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -10,6 +10,10 @@ class App extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       home: const SignInPage(),
+      // builderを使用するとNavigatorの上に、Widgetを配置することができる
+      // 従って、今回だとisLoading（OverlayLoadingの内部実装）がtrueの時には、ローディング画面がどこの画面にいても
+      // Stackの最上部に配置される。このように画面全体に特定のWidgetを適用させたり、Providerを配置することで画面全体どこからでも
+      // contextを用いてアクセル可能な状態を作ることができる（一般的には多言語化やMediaQueryなどで使用されることが多い）
       builder: ((context, navigator) {
         return Stack(
           children: [
