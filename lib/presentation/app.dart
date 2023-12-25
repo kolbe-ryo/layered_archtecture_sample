5214c497f457628f0f6fca30008b2c98c577b1cb
kolbe <water.northground@gmail.com>
2023-12-25 23:52:25 +0900

    create app.dart

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/presentation/component/overlay_loading.dart';
+import 'package:layered_archtecture_sample/presentation/page/signin_page.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return MaterialApp(
+      home: const SignInPage(),
+      builder: ((context, navigator) {
+        return Stack(
+          children: [
+            navigator!,
+            const OverlayLoading(),
+          ],
+        );
+      }),
+    );
   }
 }
