e8e1022e35f7bb364118586a09cf1d207769298d
kolbe <water.northground@gmail.com>
2023-12-28 17:45:10 +0900

    setting initial property

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
+  const HomePage({super.key})
+      : _children = [
+          const PostViewPage(),
+          const ProfilePage(),
+        ];
+
+  final List<Widget> _children;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
