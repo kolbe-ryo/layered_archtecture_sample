3086fbaf8d15ff798a61a321c7ab0ff8e51d4836
kolbe <water.northground@gmail.com>
2023-12-30 18:06:11 +0900

    BNB implememntation

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/enum/index_mode.dart';
+
+final currentIndexProvider = StateProvider<int>((ref) => IndexMode.list.index);
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key})
@@ -12,10 +15,21 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final index = :
+    final index = ref.watch(currentIndexProvider);
     return Scaffold(
       body: _children[index],
-      bottomNavigationBar: BottomNavigationBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) => ref.watch(currentIndexProvider.notifier).state = index,
+        currentIndex: index,
+        items: IndexMode.values
+            .map(
+              (e) => BottomNavigationBarItem(
+                icon: e.icon,
+                label: e.label,
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
