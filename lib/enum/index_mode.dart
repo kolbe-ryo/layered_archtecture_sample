2f728b03aba3abd6f808aef1683cb0336ac0876b
kolbe <water.northground@gmail.com>
2023-12-30 18:06:22 +0900

    index mode enum

--- lib/enum/index_mode.dart
+++ lib/enum/index_mode.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
 
 enum IndexMode {
-  
+  list('投稿リスト', Icon(Icons.list)),
+  profile('プロフィール', Icon(Icons.account_circle));
+
+  const IndexMode(this.label, this.icon);
+
+  final String label;
+  final Icon icon;
 }
