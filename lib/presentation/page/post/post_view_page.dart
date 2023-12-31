2ce570343f7d9509daaf03c304931fa156c3f8c3
kolbe <water.northground@gmail.com>
2023-12-31 21:23:35 +0900

    add appbar

--- lib/presentation/page/post/post_view_page.dart
+++ lib/presentation/page/post/post_view_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/enum/index_mode.dart';
 
 class PostViewPage extends ConsumerWidget {
   const PostViewPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(IndexMode.list.label),
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(),
+    );
   }
 }
