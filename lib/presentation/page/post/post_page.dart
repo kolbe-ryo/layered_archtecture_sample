fa2649d8bbcc218956f6d63622890548be1fefe8
kolbe <water.northground@gmail.com>
2024-01-02 21:09:12 +0900

    empty post page

---
+++ lib/presentation/page/post/post_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PostPage extends ConsumerWidget {
+  const PostPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
