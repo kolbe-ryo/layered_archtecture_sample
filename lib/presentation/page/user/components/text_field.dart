ebfc194bb8552484293668fa0ab3eaab550ff6e3
kolbe <water.northground@gmail.com>
2024-01-03 20:33:52 +0900

    password text field

--- lib/presentation/page/user/components/text_field.dart
+++ lib/presentation/page/user/components/text_field.dart
@@ -19,3 +19,20 @@ class EmailTextField extends StatelessWidget {
     );
   }
 }
+
+/// Password 入力用テキストフィールド
+class PasswordTextField extends StatelessWidget {
+  const PasswordTextField({required this.controller, super.key});
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: const InputDecoration(
+        labelText: 'パスワード',
+      ),
+      obscureText: true,
+    );
+  }
+}
