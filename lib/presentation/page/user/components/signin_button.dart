1ec73c376f2d9eab42c40b162bf36cfe3fed422d
kolbe <water.northground@gmail.com>
2024-01-04 11:26:51 +0900

    complete sign in button treatment

--- lib/presentation/page/user/components/signin_button.dart
+++ lib/presentation/page/user/components/signin_button.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/application/usecase/user/user_usecase.dart';
+import 'package:layered_archtecture_sample/presentation/page/home_page.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/email_text_field.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/password_text_field.dart';
+import 'package:layered_archtecture_sample/presentation/presentation_mixin.dart';
 
-class SignInButton extends ConsumerWidget {
+class SignInButton extends ConsumerWidget with PresentationMixin {
   const SignInButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return ElevatedButton(
+      onPressed: () async {
+        await execute(
+          context: context,
+          action: () async {
+            final navigator = Navigator.of(context);
+            await ref.read(userUsecaseProvider).signIn(
+                  email: ref.read(signinEmailControllerProvider).text,
+                  password: ref.read(signinPasswordControllerProvider).text,
+                );
+            navigator.pushReplacement(HomePage.route());
+          },
+          successMessage: '${ref.read(signinEmailControllerProvider).text} のユーザーでサインインしました',
+        );
+      },
+      child: const Text('Sign In'),
+    );
   }
 }
