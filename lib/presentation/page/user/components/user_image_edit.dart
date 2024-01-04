f1c45b17234725d23805aa5eb7a5fc443ea101b8
kolbe <water.northground@gmail.com>
2024-01-04 11:51:39 +0900

    empty user image edit widget

---
+++ lib/presentation/page/user/components/user_image_edit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserImageEdit extends ConsumerWidget {
+  const UserImageEdit({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
