e32c85f49c099c57c1a47fc6364dc37ac1365b7e
kolbe <water.northground@gmail.com>
2024-01-04 12:02:24 +0900

    create image file provider

--- lib/presentation/page/user/components/user_image_edit.dart
+++ lib/presentation/page/user/components/user_image_edit.dart
@@ -1,11 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+
+final selectedImageProvider = StateProvider.autoDispose<File?>((_) => null);
 
 class UserImageEdit extends ConsumerWidget {
   const UserImageEdit({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return CircleAvatar(
+      radius: 50,
+      backgroundImage: null,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 65.0, left: 67.5),
+        child: IconButton(
+          icon: const Icon(Icons.add_a_photo_rounded),
+          onPressed: () async {
+            final imagePicker = ImagePicker();
+            final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+            if (pickedFile == null) {
+              return;
+            }
+            // TODO: 画像の保存処理
+          },
+        ),
+      ),
+    );
   }
 }
