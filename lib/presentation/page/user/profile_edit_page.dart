3027f873caab58890465471f9b9b641764611382
kolbe <water.northground@gmail.com>
2024-01-04 11:52:38 +0900

    resolve warning

--- lib/presentation/page/user/profile_edit_page.dart
+++ lib/presentation/page/user/profile_edit_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/register_button.dart';
 import 'package:layered_archtecture_sample/presentation/page/user/components/user_image_edit.dart';
 import 'package:layered_archtecture_sample/presentation/page/user/components/user_text_field.dart';
 
@@ -16,15 +17,15 @@ class ProfileEditPage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('プロフィール更新'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: [
+          children: const [
             UserImageEdit(),
             SizedBox(height: 16.0),
             UserNameTextField(),
             SizedBox(height: 16.0),
-            // RegisterButton(),
+            RegisterButton(),
           ],
         ),
       ),
