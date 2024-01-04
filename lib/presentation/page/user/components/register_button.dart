6e64cbdaed7fccb2076521ea71dfa427849f4459
kolbe <water.northground@gmail.com>
2024-01-04 16:59:36 +0900

    fix warning

--- lib/presentation/page/user/components/register_button.dart
+++ lib/presentation/page/user/components/register_button.dart
@@ -4,6 +4,7 @@ import 'package:layered_archtecture_sample/application/usecase/user/state/user_p
 import 'package:layered_archtecture_sample/application/usecase/user/user_usecase.dart';
 import 'package:layered_archtecture_sample/enum/index_mode.dart';
 import 'package:layered_archtecture_sample/presentation/page/home_page.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/user_image_edit.dart';
 import 'package:layered_archtecture_sample/presentation/page/user/components/user_text_field.dart';
 import 'package:layered_archtecture_sample/presentation/presentation_mixin.dart';
 
@@ -21,7 +22,7 @@ class RegisterButton extends ConsumerWidget with PresentationMixin {
             await ref.read(userUsecaseProvider).register(
                   uid: ref.read(uidProvider),
                   userName: ref.read(usernameStateProvider).text,
-                  image: ref.read(selectedImage),
+                  image: ref.read(selectedImageProvider),
                 );
             ref.watch(currentIndexProvider.notifier).update((state) => IndexMode.profile.index);
             navigator.pop();
