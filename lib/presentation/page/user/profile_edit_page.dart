93541808e0b060370b4c3a01b77eb496fcedbf70
kolbe <water.northground@gmail.com>
2024-01-04 11:33:15 +0900

    router for profile  edit page

--- lib/presentation/page/user/profile_edit_page.dart
+++ lib/presentation/page/user/profile_edit_page.dart
@@ -4,6 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class ProfileEditPage extends ConsumerWidget {
   const ProfileEditPage({super.key});
 
+  static Route<void> route() {
+    return MaterialPageRoute(builder: (_) => const ProfileEditPage());
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Container();
