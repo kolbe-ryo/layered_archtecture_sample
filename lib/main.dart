5ffd3f2b1aff3ca3f2ac2581a4d0a0e9eb11baca
kolbe <water.northground@gmail.com>
2024-01-04 17:02:30 +0900

    override mock repo to main provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
+import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
+import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_storage_service.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_user_repository.dart';
 import 'package:layered_archtecture_sample/presentation/app.dart';
 
 void main() {
   runApp(
-    const ProviderScope(
+    ProviderScope(
       overrides: [
         // implement providers
+        userRepositoryProvider.overrideWithValue(MockUserRepository()),
+        postRepositoryProvider.overrideWithValue(MockPostRepository()),
+        storageServiceProvider.overrideWithValue(MockStorageService()),
       ],
-      child: App(),
+      child: const App(),
     ),
   );
 }
