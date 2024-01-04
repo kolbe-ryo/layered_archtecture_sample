b9cae59033249ddba008f7b4eb0a6ecc68c2c5b6
kolbe <water.northground@gmail.com>
2024-01-04 21:55:54 +0900

    delete todo warning

--- lib/infrastructure/remote/external_storage_service.dart
+++ lib/infrastructure/remote/external_storage_service.dart
@@ -5,7 +5,6 @@ import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
 class ExternalStorageService implements StorageService {
   @override
   Future<String> uploadImage({required File? image}) {
-    // TODO: implement uploadImage
     throw UnimplementedError();
   }
 }
--- lib/infrastructure/remote/external_user_repository.dart
+++ lib/infrastructure/remote/external_user_repository.dart
@@ -4,31 +4,26 @@ import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
 class ExternalUserRepository implements UserRepository {
   @override
   Future<void> delete({required String uid}) {
-    // TODO: implement delete
     throw UnimplementedError();
   }
 
   @override
   Future<User> fetch({required String uid}) {
-    // TODO: implement fetch
     throw UnimplementedError();
   }
 
   @override
   Future<User> register({required User user}) {
-    // TODO: implement register
     throw UnimplementedError();
   }
 
   @override
   Future<User> signIn({required String email, required String password}) {
-    // TODO: implement signIn
     throw UnimplementedError();
   }
 
   @override
   Future<String> signUp({required String email, required String password}) {
-    // TODO: implement signUp
     throw UnimplementedError();
   }
 }
