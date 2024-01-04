3d6be9f9435e071d1bb75325b5668011d08659ef
kolbe <water.northground@gmail.com>
2024-01-04 11:25:56 +0900

    implement layout for sign in and up

--- lib/presentation/page/user/signin_page.dart
+++ lib/presentation/page/user/signin_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/email_text_field.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/password_text_field.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/signin_button.dart';
+import 'package:layered_archtecture_sample/presentation/page/user/components/signup_button.dart';
 
 class SignInPage extends ConsumerWidget {
   const SignInPage({super.key});
@@ -14,7 +18,19 @@ class SignInPage extends ConsumerWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+            EmailTextField(
+              controller: ref.watch(signinEmailControllerProvider),
+            ),
+            const SizedBox(height: 16.0),
+            PasswordTextField(
+              controller: ref.watch(signinPasswordControllerProvider),
+            ),
+            const SizedBox(height: 16.0),
+            const SignInButton(),
+            const SizedBox(height: 16.0),
+            const SignUpButton(),
+          ],
         ),
       ),
     );
