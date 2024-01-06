bd100b56589a812743a1cc0542c9ffb0d04b4ec5
kolbe <water.northground@gmail.com>
2024-01-06 18:06:24 +0900

    unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:layered_archtecture_sample/application/usecase/user/user_usecase.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
 import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
 import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
