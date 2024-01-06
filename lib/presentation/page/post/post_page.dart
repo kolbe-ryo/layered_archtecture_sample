adf503d0476f9437258b6338a0ce0fc8f1f8f320
kolbe <water.northground@gmail.com>
2024-01-06 18:49:11 +0900

    complete post page

--- lib/presentation/page/post/post_page.dart
+++ lib/presentation/page/post/post_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_button.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_comment_field.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_image.dart';
 
 class PostPage extends ConsumerWidget {
   const PostPage({super.key});
@@ -13,6 +16,22 @@ class PostPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Post'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            PostImage(),
+            SizedBox(height: 16),
+            PostCommentField(),
+            SizedBox(height: 16),
+            PostButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
