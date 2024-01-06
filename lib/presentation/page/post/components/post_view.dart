22742c5cbbef41172563f46dd1e4022c9e79de36
kolbe <water.northground@gmail.com>
2024-01-06 18:35:40 +0900

    refactor post view

--- lib/presentation/page/post/components/post_view.dart
+++ lib/presentation/page/post/components/post_view.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:layered_archtecture_sample/application/usecase/post/state/post_provider.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_item.dart';
 
 class PostView extends ConsumerWidget {
   const PostView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final posts = ref.watch(postProvider);
-    return Container();
+    return ref.watch(postProvider).when(
+          data: (data) {
+            return RefreshIndicator(
+              onRefresh: () async => ref.invalidate(postProvider),
+              child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return PostItem(post: data[index]);
+                },
+              ),
+            );
+          },
+          error: (error, stack) => Center(
+            child: Text('$error'),
+          ),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
   }
 }
