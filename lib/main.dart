55942f595f448d464a6787e3f5dfba246b682244
kolbe <water.northground@gmail.com>
2024-01-06 18:06:00 +0900

    use mock user usecase if no arrangement for usecase production

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:layered_archtecture_sample/application/usecase/user/user_usecase.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
 import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
 import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
@@ -16,6 +17,7 @@ void main() {
         userRepositoryProvider.overrideWithValue(MockUserRepository()),
         postRepositoryProvider.overrideWithValue(MockPostRepository()),
         storageServiceProvider.overrideWithValue(MockStorageService()),
+        // userUsecaseProvider.overrideWithValue(MockUserUsecase()),
       ],
       child: const App(),
     ),
