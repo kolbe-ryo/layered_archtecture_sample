9353524ed8e6822f062449adced346c39e2406d1
kolbe <water.northground@gmail.com>
2024-01-06 18:43:13 +0900

    complete post item

--- lib/presentation/page/post/components/post_item.dart
+++ lib/presentation/page/post/components/post_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
+import 'package:layered_archtecture_sample/util/datetime_converter.dart';
 
 class PostItem extends StatelessWidget {
   const PostItem({
@@ -11,6 +12,31 @@ class PostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(post.user.imageUrl),
+            ),
+            title: Text(post.user.userName),
+            subtitle: Text(
+              // フォーマット等の画面表示に関するものは、ドメイン層ではなくプレゼンテーション層の責務なのでここで変換
+              post.createdAt!.toFormatDateString,
+              style: const TextStyle(fontSize: 12.0),
+            ),
+          ),
+          Image.network(
+            post.imageUrl,
+            fit: BoxFit.fill,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(post.comment),
+          ),
+        ],
+      ),
+    );
   }
 }
