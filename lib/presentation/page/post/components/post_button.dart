9aaf873848bcc6b6c936105956cfc0cafc97a157
kolbe <water.northground@gmail.com>
2024-01-07 21:17:07 +0900

    empty button and field

---
+++ lib/presentation/page/post/components/post_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class PostButton extends ConsumerWidget {
+  const PostButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
