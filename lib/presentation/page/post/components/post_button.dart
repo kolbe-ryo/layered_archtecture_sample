8a60de5666f493f0b3a65e8e8df4e27211d95daa
kolbe <water.northground@gmail.com>
2024-01-07 21:32:49 +0900

    setting arguments for add post method

--- lib/presentation/page/post/components/post_button.dart
+++ lib/presentation/page/post/components/post_button.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:layered_archtecture_sample/application/usecase/post/post_usecase.dart';
-import 'package:layered_archtecture_sample/application/usecase/post/state/post_provider.dart';
+import 'package:layered_archtecture_sample/application/usecase/user/state/user_provider.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_comment_field.dart';
+import 'package:layered_archtecture_sample/presentation/page/post/components/post_image.dart';
 import 'package:layered_archtecture_sample/presentation/presentation_mixin.dart';
 
-// TODO
 class PostButton extends ConsumerWidget with PresentationMixin {
   const PostButton({super.key});
 
@@ -17,9 +18,9 @@ class PostButton extends ConsumerWidget with PresentationMixin {
           action: () async {
             final navigator = Navigator.of(context);
             await ref.read(postUsecaseProvider).addPost(
-                  image: image,
-                  comment: comment,
-                  user: user,
+                  image: ref.read(selectedImageProvider),
+                  comment: ref.read(postCommentFieldControllerProvider).text,
+                  user: ref.read(userProvider),
                 );
             navigator.pop();
           },
