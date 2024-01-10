41fbed219b7befb8d792bc84813d05b8cb57a08f
kolbe <water.northground@gmail.com>
2024-01-10 22:19:59 +0900

    add todo for group test

--- test/domain/post_repository_test.dart
+++ test/domain/post_repository_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // TODO: add
+  // TODO: delete
+  // TODO: fetch
+  // TODO: fetchAll
+  // TODO: update
+  group('', () {});
+}
