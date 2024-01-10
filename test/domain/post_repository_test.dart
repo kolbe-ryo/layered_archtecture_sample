a05a3b9739309481896c68ba9285368c5b37b3a7
kolbe <water.northground@gmail.com>
2024-01-10 22:31:02 +0900

    todo list for post repository test case

--- test/domain/post_repository_test.dart
+++ test/domain/post_repository_test.dart
@@ -1,10 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // TODO: add
-  // TODO: delete
-  // TODO: fetch
-  // TODO: fetchAll
-  // TODO: update
-  group('', () {});
+  group('投稿取得に関するテスト', () {
+    test('存在するIDを渡すと特定の投稿が返却される', () async {});
+    test('存在しないIDを渡すとnullが返却される', () async {});
+  });
+
+  group('投稿全権取得に関するテスト', () {
+    test('', () => null);
+  });
+
+  group('投稿追加に関するテスト', () {
+    test('', () => null);
+  });
+
+  group('投稿削除に関するテスト', () {
+    test('', () => null);
+  });
+
+  group('投稿更新に関するテスト', () {
+    test('', () => null);
+  });
 }
