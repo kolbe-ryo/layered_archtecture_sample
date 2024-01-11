cba8530c1576ce58e05c992b2cf7f0a678027e9d
kolbe <water.northground@gmail.com>
2024-01-11 22:57:42 +0900

    delete test

--- test/domain/post_repository_test.dart
+++ test/domain/post_repository_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
+import 'package:layered_archtecture_sample/domain/user/entity/user.dart';
 import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
 
 void main() {
@@ -26,17 +27,36 @@ void main() {
 
   group('投稿全件取得に関するテスト', () {
     test('投稿全件が返却される', () async {
-      final post = await container.read(postRepositoryProvider).fetchAll();
-      expect(post.length, 8);
+      final posts = await container.read(postRepositoryProvider).fetchAll();
+      expect(posts.length, 8);
     });
   });
 
   group('投稿追加に関するテスト', () {
-    test('', () async {});
+    test('投稿追加するとリポジトリの投稿が追加される', () async {
+      final post = Post(
+        id: '010',
+        user: const User(
+          id: '',
+          userName: '',
+          imageUrl: '',
+        ),
+        imageUrl: '',
+        comment: '',
+        createdAt: DateTime.now(),
+      );
+      await container.read(postRepositoryProvider).add(post: post);
+      final posts = await container.read(postRepositoryProvider).fetchAll();
+      expect(posts.length, 9);
+    });
   });
 
   group('投稿削除に関するテスト', () {
-    test('', () async {});
+    test('投稿削除するとリポジトリの投稿数が減少する', () async {
+      await container.read(postRepositoryProvider).delete(postId: '001');
+      final posts = await container.read(postRepositoryProvider).fetchAll();
+      expect(posts.length, 7);
+    });
   });
 
   group('投稿更新に関するテスト', () {
