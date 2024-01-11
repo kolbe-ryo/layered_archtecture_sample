c905b46f6a2884c1ddc431bf001ddee6a7794f64
kolbe <water.northground@gmail.com>
2024-01-11 22:27:50 +0900

    fetch test

--- test/domain/post_repository_test.dart
+++ test/domain/post_repository_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:layered_archtecture_sample/domain/post/entity/post.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
 import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
 
@@ -12,12 +13,21 @@ void main() {
     ],
   );
   group('投稿取得に関するテスト', () {
-    test('存在するIDを渡すと特定の投稿が返却される', () async {});
-    test('存在しないIDを渡すとnullが返却される', () async {});
+    test('存在するIDを渡すと特定の投稿が返却される', () async {
+      final post = await container.read(postRepositoryProvider).fetch(postId: '001');
+      expect(post, isA<Post>());
+    });
+    test('存在しないIDを渡すとnullが返却される', () async {
+      final post = await container.read(postRepositoryProvider).fetch(postId: '009');
+      expect(post, isA<Null>());
+    });
   });
 
-  group('投稿全権取得に関するテスト', () {
-    test('', () => null);
+  group('投稿全件取得に関するテスト', () {
+    test('投稿', () async {
+      final post = await container.read(postRepositoryProvider).fetch(postId: '009');
+      expect(post, isA<Null>());
+    });
   });
 
   group('投稿追加に関するテスト', () {
