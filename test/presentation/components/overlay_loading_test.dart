a6dde9b44fcce21ed42d5ea89b056f755c5e872c
kolbe <water.northground@gmail.com>
2024-01-20 10:04:11 +0900

    arrange provider container

--- test/presentation/components/overlay_loading_test.dart
+++ test/presentation/components/overlay_loading_test.dart
@@ -1,1 +1,39 @@
 // TODO
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:layered_archtecture_sample/application/state/overlay_loading_provider.dart';
+import 'package:layered_archtecture_sample/presentation/components/overlay_loading.dart';
+
+void main() {
+  group('OverlayLoadingに関するテスト', () {
+    test('ローディング中ではない場合、Indicatorが表示されないこと', () {
+      final providerContainer = ProviderContainer(
+        overrides: [
+          overlayLoadingProvider.overrideWith((ref) => false),
+        ],
+      );
+    });
+    test('ローディング中の場合、Indicatorが表示されること', () {
+      final providerContainer = ProviderContainer(
+        overrides: [
+          overlayLoadingProvider.overrideWith((ref) => true),
+        ],
+      );
+    });
+  });
+}
+
+class _OverlayLoadingOutline extends StatelessWidget {
+  const _OverlayLoadingOutline();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: OverlayLoading(),
+      ),
+    );
+  }
+}
