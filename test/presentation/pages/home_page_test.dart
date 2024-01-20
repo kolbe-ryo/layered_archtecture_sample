79b5a5f8897f2809e0b0a4a28fe2656cb533992e
kolbe <water.northground@gmail.com>
2024-01-20 17:44:25 +0900

    todo for test

--- test/presentation/pages/home_page_test.dart
+++ test/presentation/pages/home_page_test.dart
@@ -1,5 +1,12 @@
-// TODO(kolbe): write test
+import 'package:flutter_test/flutter_test.dart';
+
 void main() {
-  // group('group name', () {});
+  group('レイアウトに関するテスト', () {
+    testWidgets('投稿・プロフィールの名称でBNBが表示されていること', (widgetTester) async {});
+  });
+  group('状態による表示変更に関するテスト', () {
+    testWidgets('初期表示は投稿ページが選択され表示される', (widgetTester) async {});
+    testWidgets('プロフィールのBNBをタップするとプロフィールページが表示される', (widgetTester) async {});
+  });
   // test('test name', () {});
 }
