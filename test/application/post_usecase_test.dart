b8684ca8dea6f61d23c077b83a282223a2de3874
kolbe <water.northground@gmail.com>
2024-01-20 09:51:49 +0900

    Exception test for fetchAll

--- test/application/post_usecase_test.dart
+++ test/application/post_usecase_test.dart
@@ -1,10 +1,14 @@
 // TODO:
+import 'dart:io';
+
 import 'package:collection/collection.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:layered_archtecture_sample/application/usecase/post/post_usecase.dart';
+import 'package:layered_archtecture_sample/domain/app_exception.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
 import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
+import 'package:layered_archtecture_sample/domain/user/entity/user.dart';
 import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
 import 'package:layered_archtecture_sample/infrastructure/mocks/mock_storage_service.dart';
 
@@ -28,8 +32,31 @@ void main() {
     });
   });
   group('投稿に関するテスト', () {
-    test('画像が渡されない場合、例外がthrowされる', () async {});
-    test('ユーザーが渡されない場合、例外がthrowされる', () async {});
+    const mockUser = User(id: 'id', userName: 'userName', imageUrl: 'imageUrl');
+    test('画像が渡されない場合、例外がthrowされる', () async {
+      expect(
+        () async {
+          await providerContainer.read(postUsecaseProvider).addPost(
+                image: null,
+                comment: 'comment',
+                user: mockUser,
+              );
+        },
+        throwsA(isA<AppException>()),
+      );
+    });
+    test('ユーザーが渡されない場合、例外がthrowされる', () async {
+      expect(
+        () async {
+          await providerContainer.read(postUsecaseProvider).addPost(
+                image: File('path'),
+                comment: 'comment',
+                user: null,
+              );
+        },
+        throwsA(isA<AppException>()),
+      );
+    });
     test('任意の画像・ユーザー・コメントが渡された場合、前の内容の投稿が1件追加される', () async {});
   });
 }
