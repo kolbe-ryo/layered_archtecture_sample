071058e0f2b287d861019370be62ca6476c748f2
kolbe <water.northground@gmail.com>
2024-01-20 22:54:51 +0900

    arrange App widget for initialization

--- integration_test/sign_in_test.dart
+++ integration_test/sign_in_test.dart
@@ -1,3 +1,30 @@
-// TODO(kolbe): write test
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
+import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
+import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_storage_service.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_user_repository.dart';
+import 'package:layered_archtecture_sample/presentation/app.dart';
 
-void main() {}
+void main() {
+  testWidgets('ログイン画面からサインインに成功し、プロフィールページに遷移', (widgetTester) async {
+    // Appの構築
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          userRepositoryProvider.overrideWithValue(MockUserRepository()),
+          postRepositoryProvider.overrideWithValue(MockPostRepository()),
+          storageServiceProvider.overrideWithValue(MockStorageService()),
+        ],
+        child: const App(),
+      ),
+    );
+
+    //
+  });
+
+  testWidgets('ログイン画面からサインインに成功し、プロフィールページに遷移', (widgetTester) async {});
+}
