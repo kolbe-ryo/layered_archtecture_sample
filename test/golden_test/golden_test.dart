8002e50fd8984585d1736c426db8b3c71a500dee
kolbe <water.northground@gmail.com>
2024-01-24 00:42:19 +0900

    fix bug

--- test/golden_test/golden_test.dart
+++ test/golden_test/golden_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
@@ -25,8 +26,10 @@ void main() {
       );
 
     await tester.pumpDeviceBuilder(builder);
-    await tester.pump();
-
     await screenMatchesGolden(tester, 'SignIn');
+
+    // add this:
+    await tester.pumpWidget(Container());
+    await tester.pumpAndSettle();
   });
 }
