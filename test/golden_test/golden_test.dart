8d929598faab70ba2e80e3968e167deb75ed8675
kolbe <water.northground@gmail.com>
2024-01-24 00:38:55 +0900

    generate golden image

--- test/golden_test/golden_test.dart
+++ test/golden_test/golden_test.dart
@@ -1,10 +1,32 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:layered_archtecture_sample/domain/post/post_repository.dart';
+import 'package:layered_archtecture_sample/domain/service/storage_service.dart';
+import 'package:layered_archtecture_sample/domain/user/user_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_post_repository.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_storage_service.dart';
+import 'package:layered_archtecture_sample/infrastructure/mocks/mock_user_repository.dart';
 import 'package:layered_archtecture_sample/presentation/app.dart';
 
 void main() {
   testGoldens('ログイン画面のゴールデンテスト', (tester) async {
     final builder = DeviceBuilder()
       ..overrideDevicesForAllScenarios(devices: [Device.phone, Device.iphone11])
-      ..addScenario(widget: const App(), name: 'Sign In Page');
+      ..addScenario(
+        widget: ProviderScope(
+          overrides: [
+            userRepositoryProvider.overrideWithValue(MockUserRepository()),
+            postRepositoryProvider.overrideWithValue(MockPostRepository()),
+            storageServiceProvider.overrideWithValue(MockStorageService()),
+          ],
+          child: const App(),
+        ),
+        name: 'SignIn',
+      );
+
+    await tester.pumpDeviceBuilder(builder);
+    await tester.pump();
+
+    await screenMatchesGolden(tester, 'SignIn');
   });
 }
